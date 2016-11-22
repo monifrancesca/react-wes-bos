a3a3bfcf2cb1be74cd31c89998a3616acab0f578
Moni Francesca <mo.francesca@gmail.com>
2016-11-22 21:34:07 +0100

    add on submit event that shows input value on the dom

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,15 +4,16 @@ import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
   goToStore(event) {
     event.preventDefault();
-    console.log('you changed the url & this is my first throwaway push');
+
+  console.log('you changed the url');
     // first grab the text from the box
-    console.log(this.storeInput);
+    console.log(this.storeInput.value);
     // second transition from / to /store/:storeId
   }
 
   render() {
     return (
-      <form className="store-selector" onSubmit={this.goToStore}>
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter a Store</h2>
         <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store</button>

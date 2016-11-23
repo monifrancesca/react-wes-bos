a4c6176e76b66720cafe680af72bed10a795bdeb
Moni Francesca <mo.francesca@gmail.com>
2016-11-23 21:40:58 +0100

    routing

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,22 +4,27 @@ import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
   goToStore(event) {
     event.preventDefault();
-
-  console.log('you changed the url');
+    console.log('you changed the url');
     // first grab the text from the box
-    console.log(this.storeInput.value);
+    const storeId = this.storeInput.value;
+    console.log('Going to ${storeId}');
     // second transition from / to /store/:storeId
+    this.context.router.transitionTo('/store/${storeId}');
   }
 
   render() {
     return (
-      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter a Store</h2>
         <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;

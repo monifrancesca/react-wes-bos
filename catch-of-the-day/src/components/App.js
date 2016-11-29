61c38c3d8d59ab3585180234c0253615981b37c8
Moni Francesca <mo.francesca@gmail.com>
2016-11-29 18:07:40 +0100

    load data into state using onClick

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    //getinitialState
     this.state = {
       fishes: {},
       order: {}
@@ -22,6 +25,11 @@ class App extends React.Component {
     //set state
     this.setState({ fishes });
   }
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -29,7 +37,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }

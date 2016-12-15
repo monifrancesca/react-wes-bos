69df3c1e84abd3b6a228fab0e799c6ba62e2fff4
Moni Francesca <mo.francesca@gmail.com>
2016-12-15 23:09:02 +0100

    added firebase connection. persisting state.

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyAdzoDs_pclXbHP8hCIQokEIE91AjpAybs",
+    authDomain: "catch-of-the-day-moni.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-moni.firebaseio.com"
+});
+
+export default base;
